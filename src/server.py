1886861d7fe942b1d4c4989c1a00bc9ee9d65741
CodingJaw <50494629+CodingJaw@users.noreply.github.com>
2025-11-22 02:03:10 -0500

    Add configurable uvicorn log level

--- src/server.py
+++ src/server.py
@@ -9,7 +9,8 @@ from src.main import app
 def main() -> None:
     host = os.getenv("SERVICE_HOST", "0.0.0.0")
     port = int(os.getenv("SERVICE_PORT", "8000"))
-    uvicorn.run(app, host=host, port=port)
+    log_level = os.getenv("UVICORN_LOG_LEVEL", "info")
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
 
 
 if __name__ == "__main__":
